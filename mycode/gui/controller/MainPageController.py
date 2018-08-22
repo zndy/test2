d6649486e233a71a0c887ad7994c829b478979c2
z.xin <z.xin@WKS-XIN>
2018-08-22 07:46:16 +0200

    added saveViewDataToModel and updateView

--- mycode/gui/controller/MainPageController.py
+++ mycode/gui/controller/MainPageController.py
@@ -10,12 +10,46 @@ class MainPageController:
         self.mainPage.addCalcBtnListener(self.calcBtnPressed)
 
     def calcBtnPressed(self, event):
+        self.saveViewDataToModel()
+        self.updateViewDistance()
+        self.updateViewPoints()
+
+    def saveViewDataToModel(self):
         self.mainPageModel.abricht_p1.setX(self.mainPage.abricht_p1x.get())
         self.mainPageModel.abricht_p1.setY(self.mainPage.abricht_p1y.get())
         self.mainPageModel.abricht_p2.setX(self.mainPage.abricht_p2x.get())
         self.mainPageModel.abricht_p2.setY(self.mainPage.abricht_p2y.get())
+        self.mainPageModel.diamond_p1.setX(self.mainPage.diamond_p1x.get())
+        self.mainPageModel.diamond_p1.setY(self.mainPage.diamond_p1y.get())
+        self.mainPageModel.diamond_p2.setX(self.mainPage.diamond_p2x.get())
+        self.mainPageModel.diamond_p2.setY(self.mainPage.diamond_p2y.get())
+        self.mainPageModel.diamond_p3.setX(self.mainPage.diamond_p3x.get())
+        self.mainPageModel.diamond_p3.setY(self.mainPage.diamond_p3y.get())
+        self.mainPageModel.diamond_p4.setX(self.mainPage.diamond_p4x.get())
+        self.mainPageModel.diamond_p4.setY(self.mainPage.diamond_p4y.get())
 
+    def updateViewDistance(self):
         self.mainPage.distance.set(self.mainPageModel.calcAbrichtScheibeWidth())
+
+    def updateViewPoints(self):
+        diamondPointA1 = self.mainPageModel.calcDiamondPointA1()
+        self.mainPage.diamond_a1x.set(diamondPointA1.getX())
+        self.mainPage.diamond_a1y.set(diamondPointA1.getY())
+        diamondPointA2 = self.mainPageModel.calcDiamondPointA2()
+        self.mainPage.diamond_a2x.set(diamondPointA2.getX())
+        self.mainPage.diamond_a2y.set(diamondPointA2.getY())
+        diamondPointB1 = self.mainPageModel.calcDiamondPointB1()
+        self.mainPage.diamond_b1x.set(diamondPointB1.getX())
+        self.mainPage.diamond_b1y.set(diamondPointB1.getY())
+        diamondPointB2 = self.mainPageModel.calcDiamondPointB2()
+        self.mainPage.diamond_b2x.set(diamondPointB2.getX())
+        self.mainPage.diamond_b2y.set(diamondPointB2.getY())
+        diamondPointC1 = self.mainPageModel.calcDiamondPointC1()
+        self.mainPage.diamond_c1x.set(diamondPointC1.getX())
+        self.mainPage.diamond_c1y.set(diamondPointC1.getY())
+        diamondPointC2 = self.mainPageModel.calcDiamondPointC2()
+        self.mainPage.diamond_c2x.set(diamondPointC2.getX())
+        self.mainPage.diamond_c2y.set(diamondPointC2.getY())
 
 
 def main():
--- mycode/gui/view/MainPage.py
+++ mycode/gui/view/MainPage.py
@@ -88,6 +88,16 @@ class MainPage(Frame):
         Label(self.diamondPointFrame, text='Diamond Points: ').grid(row=row, column=0, columnspan=1)
         row += 1
         self.__createResultEntry(self.diamondPointFrame, "Diamond Point A1", row, self.diamond_a1x, self.diamond_a1y)
+        row += 1
+        self.__createResultEntry(self.diamondPointFrame, "Diamond Point A2", row, self.diamond_a2x, self.diamond_a2y)
+        row += 1
+        self.__createResultEntry(self.diamondPointFrame, "Diamond Point B1", row, self.diamond_b1x, self.diamond_b1y)
+        row += 1
+        self.__createResultEntry(self.diamondPointFrame, "Diamond Point B2", row, self.diamond_b2x, self.diamond_b2y)
+        row += 1
+        self.__createResultEntry(self.diamondPointFrame, "Diamond Point C1", row, self.diamond_c1x, self.diamond_c1y)
+        row += 1
+        self.__createResultEntry(self.diamondPointFrame, "Diamond Point C2", row, self.diamond_c2x, self.diamond_c2y)
 
     def __createInputEntry(self, frame, name, row, entryX, entryY):
         Label(frame, text=name + ": ").grid(row=row, column=0, columnspan=1)
