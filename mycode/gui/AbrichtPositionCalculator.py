8c35829656d96eb626241d45c5760675f9285c5f
z.xin <z.xin@WKS-XIN>
2018-08-27 09:08:37 +0200

    updated AbrichtPositionCalculator

--- mycode/gui/AbrichtPositionCalculator.py
+++ mycode/gui/AbrichtPositionCalculator.py
@@ -26,23 +26,25 @@ class AbrichtenPositionCalculator:
             v) * MatrixUtil.translationMatrix2D(d_a / 2, f_a) * abrichtenPoint
         return left
 
-    def __getRightMatrix(self):
-        deltax, deltay, d_d, f_d = symbols('deltax deltay d_d f_d')
-        rightMatrix = MatrixUtil.translationMatrix2D(deltax - d_d / 2, deltay - f_d) * MatrixUtil.rotationMatrix2D(90)
-        return rightMatrix
+    def __calcRight(self):
+        deltax, deltay, d_d, f_d, px, py = symbols('deltax deltay d_d f_d px py')
+        diamondPoint = Matrix([px, py, 1])
+        right = MatrixUtil.translationMatrix2D(deltax - d_d / 2, deltay - f_d) * MatrixUtil.rotationMatrix2D(
+            90) * diamondPoint
+        return right
 
-    def calc(self, point=MyPoint):
+    def calc(self):
         x, y = symbols('x y')
         left = self.__calcLeft()
-        print(left)
-        right = self.__getRightMatrix() * point.toMatrix()
-        print(right)
+        print("left: " + str(left))
+        right = self.__calcRight()
+        print("right " + str(right))
         result = solve([left[0] - right[0], left[1] - right[1]], x, y)
         print(result)
 
 
 def main():
-    AbrichtenPositionCalculator().calc(MyPoint(100, 100))
+    AbrichtenPositionCalculator().calc()
 
 
 if __name__ == "__main__":
