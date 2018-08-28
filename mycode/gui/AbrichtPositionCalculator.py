b7f98a9c9cfe419758e1d3bb67f703f8617c0592
z.xin <z.xin@WKS-XIN>
2018-08-28 09:57:13 +0200

    updated test1

--- mycode/gui/AbrichtPositionCalculator.py
+++ mycode/gui/AbrichtPositionCalculator.py
@@ -9,38 +9,39 @@ from mycode.gui.BeanFactory import *
 #    d_d: Durchmesser DiamondScheibe
 #    f_a: Flansch AbrichtScheibe
 #    f_d: Flansch DiamondScheibe
-# deltaX: abstand zwischen maschine 0 und berechnung 0
-# deltaY: abstand zwischen maschine 0 und berechnung 0
+#     dx: (deltax) abstand zwischen maschine 0 und berechnung 0
+#     dy: (deltay) abstand zwischen maschine 0 und berechnung 0
+#     px: x value von AbrichtenPoint by Diamond Scheibe Koordinaten System
+#     py: y value von AbrichtenPoint by Diamond Scheibe Koordinaten System
 
 
 class AbrichtenPositionCalculator:
 
-
     def __init__(self):
         self.mainPageModel = beanFactory.mainPageModel
         self.diamondPointsPageModel = beanFactory.diamondPointsPageModel
+        self.x, self.y, self.v, self.d_a, self.f_a, self.d_d, self.f_d, self.dx, self.dy, self.px, self.py = symbols(
+            'x y v d_a f_a d_d f_d dx dy px py')
 
     def __calcLeft(self):
-        x, y, v, d_a, f_a = symbols('x y v d_a f_a')
         abrichtenPoint = Matrix([0, 0, 1])
-        left = MatrixUtil.translationMatrix2D(x, y) * MatrixUtil.rotationMatrix2D(
-            v) * MatrixUtil.translationMatrix2D(d_a / 2, f_a) * abrichtenPoint
+        left = MatrixUtil.translationMatrix2D(self.x, self.y) * MatrixUtil.rotationMatrix2D(
+            self.v) * MatrixUtil.translationMatrix2D(self.d_a / 2, self.f_a) * abrichtenPoint
         return left
 
     def __calcRight(self):
-        deltax, deltay, d_d, f_d, px, py = symbols('deltax deltay d_d f_d px py')
-        diamondPoint = Matrix([px, py, 1])
-        right = MatrixUtil.translationMatrix2D(deltax - d_d / 2, deltay - f_d) * MatrixUtil.rotationMatrix2D(
+        diamondPoint = Matrix([self.px, self.py, 1])
+        right = MatrixUtil.translationMatrix2D(self.dx - self.d_d / 2,
+                                               self.dy - self.f_d) * MatrixUtil.rotationMatrix2D(
             90) * diamondPoint
         return right
 
     def calc(self):
-        x, y = symbols('x y')
         left = self.__calcLeft()
         print("left: " + str(left))
         right = self.__calcRight()
         print("right " + str(right))
-        result = solve([left[0] - right[0], left[1] - right[1]], [x, y])
+        result = solve([left[0] - right[0], left[1] - right[1]], [self.x, self.y])
         print(result)
         return result
 
@@ -50,26 +51,29 @@ def main():
 
 
 def test1():
-    result = AbrichtenPositionCalculator().calc()
-    x, y, v, d_a, d_d, deltax, deltay, f_a, f_d, px, py = symbols('x y v d_a d_d deltax deltay f_a f_d px py')
-    xResult = result.get(x)
-    yResult = result.get(y)
-    print(xResult.subs({v: 30, d_a: 10, d_d: 20, deltax: 50, deltay: 40, f_a: 7, f_d: 8, px: 5, py: 0}).evalf())
-    print(yResult.subs({v: 30, d_a: 10, d_d: 20, deltax: 50, deltay: 40, f_a: 7, f_d: 8, px: 5, py: 0}).evalf())
-    print(xResult.subs({v: 30, d_a: 10, d_d: 20, deltax: 50, deltay: 40, f_d: 8, px: 5, py: 0}).evalf())
-    print(yResult.subs({v: 30, d_a: 10, d_d: 20, deltax: 50, deltay: 40, f_a: 7, px: 5, py: 0}).evalf())
+    calculator = AbrichtenPositionCalculator()
+    result = calculator.calc()
+    xResult = result.get(calculator.x)
+    yResult = result.get(calculator.y)
+    print(xResult.subs({calculator.v: 30, calculator.d_a: 10, calculator.d_d: 20, calculator.dx: 50, calculator.dy: 40,
+                        calculator.f_a: 7, calculator.f_d: 8, calculator.px: 5, calculator.py: 0}).evalf())
+    print(yResult.subs({calculator.v: 30, calculator.d_a: 10, calculator.d_d: 20, calculator.dx: 50, calculator.dy: 40,
+                        calculator.f_a: 7, calculator.f_d: 8, calculator.px: 5, calculator.py: 0}).evalf())
+    print(xResult.subs({calculator.v: 30, calculator.d_a: 10, calculator.d_d: 20, calculator.dx: 50, calculator.dy: 40,
+                        calculator.f_d: 8, calculator.px: 5, calculator.py: 0}).evalf())
+    print(yResult.subs({calculator.v: 30, calculator.d_a: 10, calculator.d_d: 20, calculator.dx: 50, calculator.dy: 40,
+                        calculator.f_a: 7, calculator.px: 5, calculator.py: 0}).evalf())
 
     parameters = {}
-    strv = 'v'
-    parameters[strv] = 30
-    parameters['d_a']=10
-    parameters['d_d']=20
-    parameters['deltax']=50
-    parameters['deltay']=40
-    parameters['f_a']=7
-    parameters['f_d']=8
-    parameters['px']=5
-    parameters['py']=0
+    parameters[calculator.v] = 30
+    parameters[calculator.d_a] = 10
+    parameters[calculator.d_d] = 20
+    parameters[calculator.dx] = 50
+    parameters[calculator.dy] = 40
+    parameters[calculator.f_a] = 7
+    parameters[calculator.f_d] = 8
+    parameters[calculator.px] = 5
+    parameters[calculator.py] = 0
     print(xResult.subs(parameters).evalf())
     print(yResult.subs(parameters).evalf())
 
