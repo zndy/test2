f8fd17693f5fa66ff34926bd347bf8dd4f3f12c4
z.xin <z.xin@WKS-XIN>
2018-08-30 15:44:59 +0200

    sticky=W

--- mycode/gui/view/DiamondPointsPage.py
+++ mycode/gui/view/DiamondPointsPage.py
@@ -28,71 +28,62 @@ class DiamondPointsPage(Frame):
         self.yResult = StringVar()
 
         self.row = 0
-        self.abrichtScheibeFrame = Frame(self.window)
-        self.abrichtScheibeFrame.grid(row=self.row, column=0, columnspan=2)
-        self.__createAbrichtenScheibeFrame()
+        self.abrichtScheibeFrame = self.__createAbrichtenScheibeFrame()
+        self.abrichtScheibeFrame.grid(row=self.row, column=0, columnspan=2, pady=15)
 
-        self.__createEmptyRow(self.window)
         self.row += 1
-        self.diamondPointFrame = Frame(self.window)
-        self.diamondPointFrame.grid(row=self.row, column=0, columnspan=5)
-        self.__createDiamondPointFrame()
+        self.diamondPointFrame = self.__createDiamondPointFrame()
+        self.diamondPointFrame.grid(row=self.row, column=0, columnspan=5,pady=15)
 
-        self.__createEmptyRow(self.window)
         self.row += 1
-        self.resultFrame = Frame(self.window)
-        self.resultFrame.grid(row=self.row, column=0, columnspan=2)
-        self.__createResultFrame()
+        self.resultFrame = self.__createResultFrame()
+        self.resultFrame.grid(row=self.row, column=0, columnspan=2, sticky=W,pady=15)
 
     def __createAbrichtenScheibeFrame(self):
+        abrichtScheibeFrame = Frame(self.window)
         row = 0
-        Label(self.abrichtScheibeFrame, text="Abrichten Scheibe Width: ").grid(row=row, column=0, columnspan=1)
-        Label(self.abrichtScheibeFrame, textvariable=self.abrichtScheibeWidth).grid(row=row, column=1, columnspan=1)
+        Label(abrichtScheibeFrame, text="Abrichten Scheibe Width: ").grid(row=row, column=0, columnspan=1, sticky=W)
+        Label(abrichtScheibeFrame, textvariable=self.abrichtScheibeWidth).grid(row=row, column=1, columnspan=1)
         row += 1
-        Label(self.abrichtScheibeFrame, text="Abrichten Angle: ").grid(row=row, column=0, columnspan=1)
-        Label(self.abrichtScheibeFrame, textvariable=self.abrichtAngle).grid(row=row, column=1, columnspan=1)
+        Label(abrichtScheibeFrame, text="Abrichten Angle: ").grid(row=row, column=0, columnspan=1, sticky=W)
+        Label(abrichtScheibeFrame, textvariable=self.abrichtAngle).grid(row=row, column=1, columnspan=1)
+        return abrichtScheibeFrame
 
     def __createDiamondPointFrame(self):
+        diamondPointFrame = Frame(self.window)
         row = 0
-        Label(self.diamondPointFrame, text='Diamond Points: ').grid(row=row, column=0)
+        Label(diamondPointFrame, text='Diamond Points: ').grid(row=row, column=0, sticky=W)
         row += 1
-        self.__createPointResultEntry(self.diamondPointFrame, "Diamond Point A1", row, self.diamond_a1x,
-                                      self.diamond_a1y)
+        self.__createPointResultEntry(diamondPointFrame, "Diamond Point A1", row, self.diamond_a1x, self.diamond_a1y)
         row += 1
-        self.__createPointResultEntry(self.diamondPointFrame, "Diamond Point A2", row, self.diamond_a2x,
-                                      self.diamond_a2y)
+        self.__createPointResultEntry(diamondPointFrame, "Diamond Point A2", row, self.diamond_a2x, self.diamond_a2y)
         row += 1
-        self.__createPointResultEntry(self.diamondPointFrame, "Diamond Point B1", row, self.diamond_b1x,
-                                      self.diamond_b1y)
+        self.__createPointResultEntry(diamondPointFrame, "Diamond Point B1", row, self.diamond_b1x, self.diamond_b1y)
         row += 1
-        self.__createPointResultEntry(self.diamondPointFrame, "Diamond Point B2", row, self.diamond_b2x,
-                                      self.diamond_b2y)
+        self.__createPointResultEntry(diamondPointFrame, "Diamond Point B2", row, self.diamond_b2x, self.diamond_b2y)
         row += 1
-        self.__createPointResultEntry(self.diamondPointFrame, "Diamond Point C1", row, self.diamond_c1x,
-                                      self.diamond_c1y)
+        self.__createPointResultEntry(diamondPointFrame, "Diamond Point C1", row, self.diamond_c1x, self.diamond_c1y)
         row += 1
-        self.__createPointResultEntry(self.diamondPointFrame, "Diamond Point C2", row, self.diamond_c2x,
-                                      self.diamond_c2y)
+        self.__createPointResultEntry(diamondPointFrame, "Diamond Point C2", row, self.diamond_c2x, self.diamond_c2y)
+        return diamondPointFrame
 
     def __createResultFrame(self):
+        resultFrame = Frame(self.window)
         row = 0
-        Label(self.resultFrame, text='result: ').grid(row=row, column=0)
+        Label(resultFrame, text='result: ').grid(row=row, column=0, sticky=W)
         row += 1
-        self.__createResultEntry(self.resultFrame, "x", row, self.xResult)
+        self.__createResultEntry(resultFrame, "x", row, self.xResult)
         row += 1
-        self.__createResultEntry(self.resultFrame, "y", row, self.yResult)
+        self.__createResultEntry(resultFrame, "y", row, self.yResult)
+        return resultFrame
 
     def __createPointResultEntry(self, frame, name, row, entryX, entryY):
-        Label(frame, text=name + ": ").grid(row=row, column=0, columnspan=1)
-        Label(frame, text='x:').grid(row=row, column=1, columnspan=1)
-        Label(frame, textvariable=entryX).grid(row=row, column=2, columnspan=1)
-        Label(frame, text='y:').grid(row=row, column=3, columnspan=1)
-        Label(frame, textvariable=entryY).grid(row=row, column=4, columnspan=1)
+        Label(frame, text=name + ": ").grid(row=row, column=0, columnspan=1, sticky=W)
+        Label(frame, text='x:').grid(row=row, column=1, columnspan=1, sticky=W)
+        Label(frame, textvariable=entryX).grid(row=row, column=2, columnspan=1, sticky=W)
+        Label(frame, text='y:').grid(row=row, column=3, columnspan=1, sticky=W)
+        Label(frame, textvariable=entryY).grid(row=row, column=4, columnspan=1, sticky=W)
 
     def __createResultEntry(self, frame, name, row, entry):
-        Label(frame, text=name + ": ").grid(row=row, column=0, columnspan=1)
-        Label(frame, textvariable=entry).grid(row=row, column=1, columnspan=1)
-
-    def __createEmptyRow(self, window):
-        self.row += 1
-        Label(window).grid(row=self.row, column=0)
+        Label(frame, text=name + ": ").grid(row=row, column=0, columnspan=1, sticky=W)
+        Label(frame, textvariable=entry).grid(row=row, column=1, columnspan=1, sticky=W)
