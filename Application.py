21e55a2c4ac81e950c6df0b970ddd89d352970a5
z.xin <z.xin@WKS-XIN>
2018-09-05 10:54:21 +0200

    added logger

--- Application.py
+++ Application.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
+import logging
+
 from mycode.gui.controller.MainPageController import *
 
 
 class Application:
+
     def start(self):
+        logger = logging.getLogger(__name__)
+        logger.warning("application start")
         MainPageController().openMainPage()
 
 
