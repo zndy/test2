f699768f95fb61a78106a854d08d014f27f03e80
z.xin <z.xin@WKS-XIN>
2018-09-05 10:37:28 +0200

    MyTestRunner updated

--- mycode/MyTestRunner.py
+++ mycode/MyTestRunner.py
@@ -4,11 +4,10 @@ import unittest
 class MyTestRunner:
 
     def __init__(self):
-        self.suite = unittest.TestSuite()  # 创建测试套件
+        self.suite = unittest.TestSuite()
         all_cases = unittest.defaultTestLoader.discover('.', 'test_*.py')
-        # 找到某个目录下所有的以test开头的Python文件里面的测试用例
         for case in all_cases:
-            self.suite.addTests(case)  # 把所有的测试用例添加进来
+            self.suite.addTests(case)
 
     def run(self):
         unittest.TextTestRunner(verbosity=2).run(self.suite)
